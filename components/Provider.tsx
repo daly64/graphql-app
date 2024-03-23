655ff4f95aa1c311c2ed1c1f38d5fb219655205a
Daly <dalyna64@gmail.com>
2024-03-23 23:13:30 +0100

    client

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -5,17 +5,30 @@ import {
   InMemoryCache,
   ApolloProvider,
   HttpLink,
+  DefaultOptions,
 } from "@apollo/client";
 const Provider = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const defaultOptions: DefaultOptions = {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
+    },
+  };
+
   const client = new ApolloClient({
     uri:
       "https://graphql-app-olive.vercel.app/api/graphql" ||
       "https://localhost:3000/api/graphql",
     cache: new InMemoryCache(),
+    defaultOptions: defaultOptions,
   });
 
   /*   const client = new ApolloClient({
