5026cbcbd8bf06f644258bef46561907f90824df
Daly <dalyna64@gmail.com>
2024-03-23 22:14:54 +0100

    up 13

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,17 +1,35 @@
 "use client";
 import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
 const Provider = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  // const client = new ApolloClient({
+  // uri:
+  //   "http://localhost:3000/api/graphql",
+  //   uri: "https://graphql-app-olive.vercel.app/api/graphql",
+
+  //  cache: new InMemoryCache(),
+
+  // });
+
   const client = new ApolloClient({
-    // uri:
-    //   "http://localhost:3000/api/graphql",
-    uri: "https://graphql-app-olive.vercel.app/api/graphql",
+    link: new HttpLink({
+      uri: "https://graphql-app-olive.vercel.app/api/graphql",
+      fetchOptions: {
+        mode: "no-cors",
+      },
+    }),
     cache: new InMemoryCache(),
   });
+
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
 
